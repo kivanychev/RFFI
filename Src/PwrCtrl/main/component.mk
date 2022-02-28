6842d51e80a2bbead7361be75c38dfed401b1933
kivanych <kirill.ivanychev@zorgvision.ru>
2022-02-28 22:00:23 +0300

    HTTP Server with controlled LED on GPIO13

--- Src/PwrCtrl/main/component.mk
+++ Src/PwrCtrl/main/component.mk
@@ -1,8 +1,10 @@
 #
-# Main component makefile.
+# Main Makefile. This is basically the same as a component makefile.
 #
-# This Makefile can be left empty. By default, it will take the sources in the
-# src/ directory, compile them and link them into lib(subdirectory_name).a
-# in the build directory. This behaviour is entirely configurable,
-# please read the ESP-IDF documents if you need to do this.
+# This Makefile should, at the very least, just include $(SDK_PATH)/make/component.mk. By default, 
+# this will take the sources in the src/ directory, compile them and link them into 
+# lib(subdirectory_name).a in the build directory. This behaviour is entirely configurable,
+# please read the SDK documents if you need to do this.
 #
+COMPONENT_EMBED_FILES := www/index_ov2640.html.gz
+COMPONENT_EMBED_FILES += www/monitor.html.gz
