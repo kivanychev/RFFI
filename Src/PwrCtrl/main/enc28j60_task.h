35433ff34b021e118f9412433cf54b364459638f
kivanych <kirill.ivanychev@zorgvision.ru>
2022-04-08 16:24:43 +0300

    Added UART and Ethernet Enc28J60 functionality

---
+++ Src/PwrCtrl/main/enc28j60_task.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// ================================================
+//                    CONSTANTS
+// ================================================
+
+#define CONFIG_EXAMPLE_ENC28J60_SPI_HOST 1
+#define CONFIG_EXAMPLE_ENC28J60_SCLK_GPIO 19
+#define CONFIG_EXAMPLE_ENC28J60_MOSI_GPIO 23
+#define CONFIG_EXAMPLE_ENC28J60_MISO_GPIO 25
+#define CONFIG_EXAMPLE_ENC28J60_CS_GPIO 22
+#define CONFIG_EXAMPLE_ENC28J60_SPI_CLOCK_MHZ 6
+#define CONFIG_EXAMPLE_ENC28J60_INT_GPIO 4
+#define CONFIG_EXAMPLE_ENC28J60_DUPLEX_HALF 1
+
+// ================================================
+//                FUNCTION PROTOTYPES
+// ================================================
+
+void enc28j60_start_task(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
---
+++ Src/PwrCtrl/main/uart_task.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void uart_start_task(void);
+
+
+#ifdef __cplusplus
+}
+#endif
